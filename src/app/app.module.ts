d66c6b1294482d537873d30a71deb63c27a6ba3b
Kazi <kazi.shahinbd@gmail.com>
2023-04-07 22:06:18 +0200

    mapbox integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MapContainerComponent } from './map-container/map-container.component';
+import { StoreModule } from '@ngrx/store';
+import { NgrxModule } from './store/ngrx.module';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgrxModule,
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapBox: {
+    accessToken: 'pk.eyJ1Ijoia2F6aTR1MDAwIiwiYSI6ImNsZzJlcHZpZDA0dHYzbm1wc2FsYjZzdDcifQ.wnPHRU5WIORJ6gNoDSElrg'
+  },
+  mapTiler: {
+    accessToken: 'pXj7ISzIvAgYUgPS5nxk'
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
